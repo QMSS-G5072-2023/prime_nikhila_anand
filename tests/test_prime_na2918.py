ea1b641e772da2b99854c136ba0a3812235a2912
Nikhila Anand <na2918@columbia.edu>
2023-10-31 19:08:00 -0400

    Added tests and pytest dependency

--- tests/test_prime_na2918.py
+++ tests/test_prime_na2918.py
@@ -1,1 +1,23 @@
-from prime_na2918 import prime_na2918
+from prime_na2918 import prime_na2918 as primes
+
+def test_is_prime_param():
+    '''
+        Test the primes.is_prime function for a list of examples, 
+including
+        negative numbers, 0, and 1
+    '''
+    examples = [
+        (2, True),
+        (7, True),
+        (8, False),
+        (9, False),
+        (-1, False),
+        (-4, False),
+        (0, False),
+        (1, False),
+    ]
+
+    for example, expected in examples:
+        result = primes.is_prime(example)
+        assert result == expected
+
